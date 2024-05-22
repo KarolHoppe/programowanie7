c4c4dfa5eae7faaf7e23cf8e9033817f945c26ef
KarolHoppe <162447146+KarolHoppe@users.noreply.github.com>
2024-05-22 10:58:02 +0200

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+    public double bokA;
+    public double bokB;
+
+    public Rectangle(double bokA, double bokB) {
+        this.bokA = bokA;
+        this.bokB = bokB;
+    }
+    public double getObwod() {
+        return 2*bokA + 2*bokB;
+    }
+
+    public double getPole(){
+        return bokA*bokB;
+    }
+    public String getTyp(){
+        return "Rectangle";
+    }
+}
